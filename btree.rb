21a18f94613f113f7ad5486b3b9f217fe10074b3
naa59 <log283@yandex.ru>
2013-12-01 18:47:02 +0400

    01.12.2013

--- btree.rb
+++ btree.rb
@@ -1,3 +1,6 @@
+require 'rgl/adjacency'
+require 'rgl/dot'
+
 class Btree
 
 	class Tree
@@ -28,6 +31,10 @@ class Btree
 		end
 	end
 	
+	def show
+		@root.show
+	end
+	
 	def search(key,node = root)
 		i=1
 		while i<=node.size && key > node.keys[i-1]
@@ -44,7 +51,19 @@ class Btree
 	end
 	
 	def delete(key)
-		@root.delete(key)
+		if !search(key)
+			return nil
+		else
+			@root.delete(key)
+		end
+	end
+
+	def visualize
+		if @root.size<=0
+			return nil
+		else
+			@root.visualize
+		end	
 	end
 
 	def degree
@@ -67,6 +86,18 @@ class Btree
     		@children = []
     		@leaf = true
   	end
+	
+	def show
+		childrenarray=[]
+		puts "#{keys}"
+		print "("
+		children.each do |c|
+			print "#{c.keys}"
+			childrenarray << c
+		end
+		print ")\n"
+		show_helper(childrenarray)
+	end
 	
 	def insert(key)
 		i=size-1
@@ -122,7 +153,126 @@ class Btree
 			child.children.delete(j)
 		end
 	end
+	
+	def delete(key)
+		i=0
+		while i<size && key > keys[i]
+			i+=1
+		end
+		if  i<=size && key == keys[i]
+			if leaf?
+				return keys.delete(key)
+			elsif children[i].size>=@degree
+					keys[i]=children[i].fbig
+					return children[i].delete(children[i].fbig)
+			elsif children[i+1].size>=@degree
+					keys[i]=children[i+1].fsmall
+					return children[i+1].delete(children[i+1].fsmall)
+			else 		
+					children[i].keys << key
+					(@degree-1).times do |j|
+						children[i].keys << children[i+1].keys[j]
+					end
+					children[i+1].children.size.times do |j|
+						children[i].children << children[i+1].children[j]
+					end
+					keys.delete(key)
+					if @keys.empty?
+						@degree=children[i].degree
+						@leaf=children[i].leaf?
+						@keys=children[i].keys
+						@children=children[i].children
+						return delete(key)
+					else
+						children.delete_at(i+1)
+						return children[i].delete(key)
+					end
+			end
+		end
+		if leaf?
+			return nil
+		end
+		if children[i].size<@degree
+			if children[i-1] && i!=0 && children[i-1].size>=@degree				
+				children[i].keys.unshift(keys[i-1])
+				keys[i-1]=children[i-1].keys[-1]
+				children[i-1].keys.delete_at(-1)
+				if children[i-1].children[-1]
+					children[i].children.unshift(children[i-1].children[-1])
+					children[i-1].children.delete_at(-1)
+				end
+				return children[i].delete(key)
+			elsif children[i+1] && children[i+1].size>=@degree
+				children[i].keys << (keys[i])
+				keys[i]=children[i+1].keys[0]
+				children[i+1].keys.delete_at(0)
+				if children[i+1].children[0]
+					children[i].children << children[i+1].children[0]
+					children[i+1].children.delete_at(0)
+				end
+				return children[i].delete(key)
+			elsif children[i-1] && i!=0
+				children[i].keys.size.times do |j|
+					children[i-1].keys << (children[i].keys[j])
+				end
+				if children[i].children.size>0
+					children[i].children.size.times do |j|
+						children[i-1].children << (children[i].children[j])
+					end
+				end
+				children[i-1].keys.insert(@degree-1,keys[i-1])
+				children.delete_at(i)
+				keys.delete_at(i-1)
+				if @keys.empty?
+					@degree=children[i-1].degree
+					@leaf=children[i-1].leaf?
+					@keys=children[i-1].keys
+					@children=children[i-1].children
+					return delete(key)
+				else
 
+					return children[i-1].delete(key)
+				end
+			elsif children[i+1]
+				children[i+1].keys.size.times do |j|
+					children[i].keys << children[i+1].keys[j]
+				end
+				if children[i+1].children.size>0
+				children[i+1].children.size.times do |j|
+					children[i].children << children[i+1].children[j]
+				end
+				end
+				children[i].keys.insert(@degree-1,keys[i])
+				children.delete_at(i+1)
+				keys.delete_at(i)
+				if @keys.empty?
+					@degree=children[i].degree
+					@leaf=children[i].leaf?
+					@keys=children[i].keys
+					@children=children[i].children
+					return delete(key)
+				else
+					return children[i].delete(key)
+				end
+			end
+		end
+		return children[i].delete(key)
+	end
+
+	def visualize
+		childrenarray=[]
+		vtree=RGL::DirectedAdjacencyGraph.new
+		if leaf?
+			vtree.add_vertex(keys)
+			return vtree
+		else
+		children.each do |c|
+			vtree.add_edge(keys,c.keys)
+			childrenarray << c
+		end
+		end
+		return vishelper(vtree,childrenarray)
+	end
 
 	def leaf?
 		@leaf
@@ -139,6 +289,7 @@ class Btree
 	def degree
 		@degree
 	end
+
 	def size
     		@keys.size
   	end
@@ -147,6 +298,105 @@ class Btree
     		size >= 2 * @degree - 1
   	end
 
+	def fsmall
+		if leaf?
+			return keys[0]
+		else
+			return children[0].fsmall
+		end
 	end
 
+	def fbig
+		if leaf?
+			return keys[-1]
+		else
+			return children[-1].fbig
+		end
+	end
+
+	end
+
+end
+
+def generate_btree(keys=10,degree=2,limit=20)
+	tree=Btree::Tree.new(degree)
+		keys.times do
+			tree.insert(rand(limit))
+		end
+	return tree
+end
+
+def show_helper(array)
+	if array[0] && !array[0].leaf?
+		temp=[]
+		array.each do |c| 
+			print "("
+				c.children.each do |cc|
+					print "#{cc.keys}"
+					temp << cc
+				end
+			print ")"
+		end
+		puts
+		return show_helper(temp)
+	end
+end
+
+def vishelper(tree,array)
+	if array[0] && !array[0].leaf?
+		temp=[]
+		array.each do |c|
+			c.children.each do |cc|
+				tree.add_edge(c.keys,cc.keys)
+				temp << cc
+			end
+		end
+		return vishelper(tree,temp)
+	else
+		return tree
+	end
+end
+
+puts "Input minimal degree(default: 2):"
+degree = gets.chomp.to_i
+grnum=1
+if !degree || degree<2
+	puts "Invalid degree: #{degree}. Using degree: 2 instead" 
+	tree=Btree::Tree.new
+	else
+	tree=Btree::Tree.new(degree)
+end
+puts "To see the list of actions input action '?'"
+while true
+	puts "Input action" 
+	action=gets.chomp.to_s
+	if action == "i"
+		puts "Input key to insert:"
+		key=gets.chomp.to_i
+		tree.insert(key)
+	elsif action == "d"
+		puts "Input key to delete:"
+		key=gets.chomp.to_i
+		tree.delete(key)
+	elsif action == "s"
+		puts "Input key to search for:"
+		key=gets.chomp.to_i
+		puts tree.search(key).inspect
+	elsif action == "t"
+		puts
+		tree.show
+		puts
+	elsif action == "v"
+		vtree=tree.visualize
+		vtree.write_to_graphic_file('jpg')
+		system( "mkdir -p ./graphs" )
+		system( "rm -rf graph.dot" )
+		system( "mv graph.jpg ./graphs/graph#{grnum}.jpg" )
+		puts "Created graph#{grnum}.jpg file\n"
+		grnum+=1
+	elsif action == "?"
+		puts "'i' to Insert\n'd' to Delete\n's' to Search\n't' to Show the tree\n'v' to Visualize the tree"
+	else
+		puts "Unknown action: '#{action}'\nTo see the list of actions input action '?'"
+	end
 end
